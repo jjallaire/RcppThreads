46c490622c0fb62a1b1e0e00b3ce6f36b20dfb07
JJ Allaire <jj@rstudio.org>
2014-07-03 18:42:53 -0700

    more work

--- inst/examples/hello_threads.cpp
+++ inst/examples/hello_threads.cpp
@@ -1,13 +1,55 @@
-
-
 #include <Rcpp.h>
 using namespace Rcpp;
 
 // [[Rcpp::depends(RcppThreads)]]
 #include <RcppThreads.h>
+using namespace RcppThreads;
+
+#include <cmath>
+
+
+
+struct SqrtBody : public Body
+{
+   // source matrix
+   double * const input;
+   
+   // destination matrix
+   double* output;
+   
+   // initialize with source and destination
+   SqrtBody(double * const input, double* output) 
+      : input(input), output(output) {}
+   
+   // take the square root of the range of elements requested
+   void operator()(const IndexRange& range) const {
+      std::transform(input + range.begin(),
+                     input + range.end(),
+                     output + range.begin(),
+                     ::sqrt);
+   }
+};
 
 
 // [[Rcpp::export]]
-unsigned hardwareConcurrency() {
-  return tthread::thread::hardware_concurrency();
+NumericVector squareRoot(NumericVector x) {
+  
+  NumericVector output(x.length());
+  
+  SqrtBody body(x.begin(), output.begin());
+  
+  parallelFor(x, body);
+    
+  return output;
 }
+
+
+/*** R
+
+vec <- as.numeric(c(1:1000))
+
+squareRoot(vec)
+*/
+
+
+
--- inst/include/RcppThreads.h
+++ inst/include/RcppThreads.h
@@ -1,6 +1,93 @@
 
+#ifndef __RCPP_THREADS__
+#define __RCPP_THREADS__
 
+// tinythread library 
 #include "tthread/tinythread.h"
 #include "tthread/tinythread.inl"
 
+namespace RcppThreads {
 
+// Class which represents a range of indexes to perform work on
+// (worker functions are passed this range so they know which
+// elements are safe to read/write to)
+class IndexRange {
+public:
+
+  // Initizlize with a begin and (exclusive) end index
+  IndexRange(std::size_t begin, std::size_t end)
+    : begin_(begin), end_(end)
+  {
+  }
+  
+  // Initialize with a type that has a length() member 
+  // (begin is always 0 for this constructor)
+  template <typename Indexable>
+  IndexRange(const Indexable& indexable) 
+    : begin_(0), end_(indexable.length())
+  {
+  }
+  
+  // Access begin() and end()
+  std::size_t begin() const { return begin_; }
+  std::size_t end() const { return end_; }
+  
+private:
+  std::size_t begin_;
+  std::size_t end_;
+};
+
+
+// Body of code to execute within a worker thread
+class Body {
+public:
+  virtual ~Body() {} 
+  virtual void operator()(const IndexRange& range) const = 0;
+};
+
+
+namespace {
+
+// Because tinythread allows us to pass only a plain C function
+// we need to pass our body and range within a struct that we 
+// can cast to/from void*
+struct Work {
+  Work(IndexRange range, const Body& body) 
+    :  range(range), body(body)
+  {
+  }
+  IndexRange range;
+  const Body& body;
+};
+
+// Thread which performs work (then deletes the work object
+// when it's done)
+extern "C" void workerThread(void* data) {
+  try
+  {
+    Work* pWork = static_cast<Work*>(data);
+    pWork->body(pWork->range);
+    delete pWork;
+  }
+  catch(...)
+  {
+  }
+}  
+  
+} // anonymous namespace
+
+
+// Execute the Body over the IndexRange in parallel
+void parallelFor(IndexRange range, const Body& body) {
+  
+  // for now we have a simple serial implementation...
+  
+  tthread::thread t(workerThread, static_cast<void*>(new Work(range, body)));
+  t.join();
+}
+
+
+
+} // namespace RcppThreads
+
+#endif // __RCPP_THREADS__
